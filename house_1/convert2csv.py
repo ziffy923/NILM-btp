b50fffe783b817088a9e88f47ec4317686ebdedf
ziffy923 <mahaprasad923@gmail.com>
2019-08-16 21:59:31 +0530

    to convert file to csv from .dat

---
+++ house_1/convert2csv.py
@@ -0,0 +1,31 @@
+import os
+import pandas as pd
+# al=os.system('ls')
+# print(al)
+print('hshshshshshsh')
+os.system('ls')
+
+path = '/home/ziffy/low_freq_copy/house_1'
+
+files = []
+# r=root, d=directories, f = files
+for r, d, f in os.walk(path):
+    for file in f:
+        if '.dat' in file:
+            files.append(file)
+
+data_dict={}
+
+for i in range(len(files)):
+    timestamp=[]
+    value=[]
+    f=open(files[i],'r')
+    data=f.readlines()
+    for line in data:
+        word=line.split()
+        timestamp.append(int(word[0]))
+        value.append(float(word[1]))
+    df=pd.DataFrame({'value':value},index=timestamp)
+    df.to_csv('Data_csv/'+files[i]+".csv")
+    f.close()
+    print('completed',files[i])
