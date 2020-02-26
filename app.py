c32835c3110b645a728df8dfd22fa5567a84872f
iml-v <bishnoimlv@gmail.com>
2020-02-26 15:01:26 +0530

    models files added

--- app.py
+++ app.py
@@ -5,11 +5,10 @@ import pickle
 import numpy as np
 # from keras.models import load_model
 # import h5py
-
+#
 # prd_model = load_model('model-rc.hdf5')
 
 model = pickle.load(open('model.pkl','rb'))
-
 
 app = Flask(__name__)
 
